517264a6bd5f5ccf91d013595c8720145f8da7e2
grdvnl <grdvnl@gmail.com>
2021-01-13 19:46:31 -0800

    Update configobj versio to align with pgcli

--- pgspecial/iocommands.py
+++ pgspecial/iocommands.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from contextlib import contextmanager
 import re
+import fnmatch
 import sys
 import logging
 import click
--- tests/test_specials.py
+++ tests/test_specials.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
   #!/usr/bin/python
   # -*- coding: utf-8 -*-
 
