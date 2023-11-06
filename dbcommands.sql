653da8cb2e8d254c56252dcbdc4849dae8fd06f6
Daniel Kukula <daniel.kuku@gmail.com>
2023-11-06 20:06:24 +0000

    list foreign tables

--- dbcommands.sql
+++ dbcommands.sql
@@ -636,15 +636,33 @@ pg_catalog.pg_get_functiondef(
     END
 ) AS source
 
+-- name:  list_foreign_tables_verbose
+-- docs: ("\\dE", "\\dE[+] [pattern]", "List foreign tables.", aliases=())
+SELECT
+    n.nspname AS "Schema",
+    c.relname AS "Name",
+    CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' WHEN 'm' THEN 'materialized view' WHEN 'i' THEN 'index' WHEN 'S' THEN 'sequence' WHEN 's' THEN 'special' WHEN 'f' THEN 'foreign table' WHEN 'p' THEN 'table' WHEN 'I' THEN 'index' END as "Type",
+    pg_catalog.pg_get_userbyid(c.relowner) AS "Owner",
+    pg_catalog.pg_size_pretty(pg_catalog.pg_table_size(c.oid)) AS "Size",
+    pg_catalog.obj_description(c.oid, 'pg_class') AS "Description"
+FROM
+    pg_catalog.pg_class c
+    LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+WHERE
+    c.relkind IN ('f', '')
+    AND n.nspname <> 'pg_catalog'
+    AND n.nspname <> 'information_schema'
+    AND n.nspname !~ '^pg_toast'
+    AND pg_catalog.pg_table_is_visible(c.oid)
+    AND c.relname OPERATOR (pg_catalog. ~) :pattern
+ORDER BY 1, 2
 -- name:  list_foreign_tables
 -- docs: ("\\dE", "\\dE[+] [pattern]", "List foreign tables.", aliases=())
 SELECT
-    n.nspname AS "schema",
-    c.relname AS "name",
-    :relkind AS "type",
-    pg_catalog.pg_get_userbyid(c.relowner) AS "owner",
-    pg_catalog.pg_size_pretty(pg_catalog.pg_table_size(c.oid)) AS "size",
-    pg_catalog.obj_description(c.oid, 'pg_class') AS "description"
+    n.nspname AS "Schema",
+    c.relname AS "Name",
+    CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' WHEN 'm' THEN 'materialized view' WHEN 'i' THEN 'index' WHEN 'S' THEN 'sequence' WHEN 's' THEN 'special' WHEN 'f' THEN 'foreign table' WHEN 'p' THEN 'table' WHEN 'I' THEN 'index' END as "Type",
+    pg_catalog.pg_get_userbyid(c.relowner) AS "Owner"
 FROM
     pg_catalog.pg_class c
     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
--- pgspecial/dbcommands.py
+++ pgspecial/dbcommands.py
@@ -1640,46 +1640,12 @@ def shell_command(cur, pattern, verbose):
 
 @special_command("\\dE", "\\dE[+] [pattern]", "List foreign tables.", aliases=())
 def list_foreign_tables(cur, pattern, verbose):
-    params = {}
-    query = SQL(
-        """
-        SELECT n.nspname as "Schema",
-        c.relname as "Name",
-        CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' WHEN 'm' THEN 'materialized view' WHEN 'i' THEN 'index' WHEN 'S' THEN 'sequence' WHEN 's' THEN 'special' WHEN 'f' THEN 'foreign table' WHEN 'p' THEN 'table' WHEN 'I' THEN 'index' END as "Type",
-        pg_catalog.pg_get_userbyid(c.relowner) as "Owner"
-        {verbose_cols}
-        FROM pg_catalog.pg_class c
-            LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-        WHERE c.relkind IN ('f','')
-            AND n.nspname <> 'pg_catalog'
-            AND n.nspname <> 'information_schema'
-            AND n.nspname !~ '^pg_toast'
-        AND pg_catalog.pg_table_is_visible(c.oid)
-        {filter}
-        ORDER BY 1,2;
-        """
-    )
-
+    _, tbl_name = sql_name_pattern(pattern)
+    pattern = f"^({tbl_name})$" if tbl_name else ".*"
     if verbose:
-        params["verbose_cols"] = SQL(
-            """
-            , pg_catalog.pg_size_pretty(pg_catalog.pg_table_size(c.oid)) as "Size",
-            pg_catalog.obj_description(c.oid, 'pg_class') as "Description" """
-        )
+        cur.execute(queries.list_foreign_tables_verbose.sql, {"pattern": pattern})
     else:
-        params["verbose_cols"] = SQL("")
-
-    if pattern:
-        _, tbl_name = sql_name_pattern(pattern)
-        params["filter"] = SQL(" AND c.relname OPERATOR(pg_catalog.~) {} ").format(
-            f"^({tbl_name})$"
-        )
-    else:
-        params["filter"] = SQL("")
-
-    formatted_query = query.format(**params)
-    log.debug(formatted_query.as_string(cur))
-    cur.execute(formatted_query)
+        cur.execute(queries.list_foreign_tables.sql, {"pattern": pattern})
     if cur.description:
         headers = [x.name for x in cur.description]
         return [(None, cur, headers, cur.statusmessage)]
